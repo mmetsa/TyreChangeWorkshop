6e2b346c58b105d034eddb53680fa26d3f28a49a
Maiko Metsalu <metsalumaiko@gmail.com>
2022-09-11 20:38:09 +0300

    Initial commit

---
+++ src/main/java/com/tyrechangeworkshop/backend/TyreChangeWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.tyrechangeworkshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyreChangeWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyreChangeWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tyrechangeworkshop/backend/TyreChangeWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyrechangeworkshop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyreChangeWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
