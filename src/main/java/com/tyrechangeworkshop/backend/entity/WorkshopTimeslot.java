afb94bc350d046c1492d9b1bc09abb3474eb0295
Maiko Metsalu <metsalumaiko@gmail.com>
2022-09-16 23:07:48 +0300

    Added start and end date to intermediary table

--- src/main/java/com/tyrechangeworkshop/backend/entity/WorkshopTimeslot.java
+++ src/main/java/com/tyrechangeworkshop/backend/entity/WorkshopTimeslot.java
@@ -1,6 +1,7 @@
 package com.tyrechangeworkshop.backend.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "workshop_timeslot")
@@ -9,6 +10,12 @@ public class WorkshopTimeslot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
 
     @ManyToOne
     private Workshop workshop;
